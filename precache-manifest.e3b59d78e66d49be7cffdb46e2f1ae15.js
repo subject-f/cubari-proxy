238f44c4cdf29711f2a778c2c3331a9a11a572d9
funkyhippo <funkyhippo@users.noreply.github.com>
2020-08-16 14:45:26 +0000

    deploy: 51508d2ce6852cbd135db9400543a42bb152ce2c

---
+++ precache-manifest.e3b59d78e66d49be7cffdb46e2f1ae15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0596b524bea4dcc779edb770ce9f957c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec5185efc49fe304bf9d",
+    "url": "/static/css/main.9b0bc2f7.chunk.css"
+  },
+  {
+    "revision": "6335f1d0d8dc7b4a7f8f",
+    "url": "/static/js/2.ddcf6d5f.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/static/js/2.ddcf6d5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec5185efc49fe304bf9d",
+    "url": "/static/js/main.343e9935.chunk.js"
+  },
+  {
+    "revision": "c656b6308aa4bb58176d",
+    "url": "/static/js/runtime-main.d0742a87.js"
+  }
+]);
