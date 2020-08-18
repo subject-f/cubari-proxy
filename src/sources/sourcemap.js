1f6b5740ddd4f8eceff5fcf6299f1d2f8f1a6b48
funkyhippo <funkyhippo@f-ck.me>
2020-08-17 17:45:49 -0700

    Hentai only.

--- src/sources/sourcemap.js
+++ src/sources/sourcemap.js
@@ -4,13 +4,13 @@ import NHentai from "./NHentai";
 
 const hentai = localStorage.getItem("hentai");
 
-const sourcemap = {
-  MangaDex: new MangaDex(),
-  Guya: new Guya(),
-};
+const sourcemap = {};
 
 if (hentai || window.location.search.includes("hentai")) {
   sourcemap["NHentai"] = new NHentai();
+} else {
+  sourcemap["MangaDex"] = new MangaDex();
+  sourcemap["Guya"] = new Guya();
 }
 
 export default sourcemap;
