158f4789e68ebf5bcaef204eafd06d1f42ffce69
funkyhippo <funkyhippo@f-ck.me>
2021-05-15 15:01:23 -0700

    Fade transition for manga cards.

--- src/components/MangaCard.js
+++ src/components/MangaCard.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from "react";
 import observer from "../utils/observer";
 import { insert } from "../utils/history";
+import { Transition } from "@headlessui/react";
 
 export default class MangaCard extends PureComponent {
   constructor(props) {
@@ -31,31 +32,42 @@ export default class MangaCard extends PureComponent {
 
   render() {
     return (
-      <div className="px-3">
-        <a
-          ref={this.ref}
-          href={this.props.mangaUrlizer(this.props.slug)}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="bg-no-repeat bg-cover bg-center bg-gray-300 dark:bg-gray-800 transform rounded-lg shadow-md scale-100 md:hover:scale-105 h-72 w-48 flex flex-row flex-wrap p-1 transition duration-100 ease-in-out"
-          data-background-image={`linear-gradient(rgba(0,0,0,0) 50%, rgba(0,0,0,0.8) 90%), url("${this.props.coverUrl}")`}
-          onMouseDown={this.saveToHistory}
-        >
-          <div className="w-full h-full px-0 flex flex-row flex-wrap overflow-hidden">
-            <div className="w-full text-gray-700 font-semibold relative pt-3 md:pt-0">
-              <div
-                className="text-l text-white absolute bottom-0 left-0 mx-1 mb-1"
-                style={{
-                  textShadow:
-                    "0 0 4px black, 0 0 4px black, 0 0 4px black, 0 0 4px black",
-                }}
-              >
-                {this.props.mangaTitle}
+      <Transition
+        appear={true}
+        show={true}
+        enter="transition-opacity duration-500"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity duration-500"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div className="px-3">
+          <a
+            ref={this.ref}
+            href={this.props.mangaUrlizer(this.props.slug)}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-no-repeat bg-cover bg-center bg-gray-300 dark:bg-gray-800 transform rounded-lg shadow-md scale-100 md:hover:scale-105 h-72 w-48 flex flex-row flex-wrap p-1 transition duration-100 ease-in-out"
+            data-background-image={`linear-gradient(rgba(0,0,0,0) 50%, rgba(0,0,0,0.8) 90%), url("${this.props.coverUrl}")`}
+            onMouseDown={this.saveToHistory}
+          >
+            <div className="w-full h-full px-0 flex flex-row flex-wrap overflow-hidden">
+              <div className="w-full text-gray-700 font-semibold relative pt-3 md:pt-0">
+                <div
+                  className="text-l text-white absolute bottom-0 left-0 mx-1 mb-1"
+                  style={{
+                    textShadow:
+                      "0 0 4px black, 0 0 4px black, 0 0 4px black, 0 0 4px black",
+                  }}
+                >
+                  {this.props.mangaTitle}
+                </div>
               </div>
             </div>
-          </div>
-        </a>
-      </div>
+          </a>
+        </div>
+      </Transition>
     );
   }
 }
