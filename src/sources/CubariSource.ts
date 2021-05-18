2c35343610dd146da7c8dc453799fc0483c0b0ea
funkyhippo <funkyhippo@f-ck.me>
2021-05-17 19:57:04 -0700

    Remove unsafe headers from requests.

--- src/sources/CubariSource.ts
+++ src/sources/CubariSource.ts
@@ -1,9 +1,15 @@
 import { SourceInfo } from "paperback-extensions-common";
 import axios, { AxiosResponse, AxiosRequestConfig } from "axios";
 
+const UNSAFE_HEADERS = new Set(["cookie", "user-agent"]);
+
 const requestInterceptor = (req: AxiosRequestConfig) => {
-  const request = req;
-  request.url = `https://cors.bridged.cc/${request.url}${request.params ?? ""}`;
+  req.url = `https://cors.bridged.cc/${req.url}${req.params ?? ""}`;
+  Object.keys(req.headers).forEach((header) => {
+    if (UNSAFE_HEADERS.has(header.toLowerCase())) {
+      delete req.headers[header];
+    }
+  });
   return req;
 };
 
